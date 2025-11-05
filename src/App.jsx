b751e64e13cff29a4317d49cbf86e78edc80176b
HimanshuTech0509 <himanshu736raj@gmail.com>
2025-11-05 13:35:40 +0530

    feat: add mock data for StudentDetails page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
 import { NavBar } from './Components/NavBar'
+import { HomePage } from './Pages/HomePage'
 
 function App(){
  
 
   return (
     <>
-
-    
     <NavBar></NavBar> 
-   
+    <HomePage/>
     
     </>
     
